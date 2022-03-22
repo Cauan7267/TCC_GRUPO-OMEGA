fa77cae9dc7c0965f1d8279bc2543fa4445ec446
cauan <cauan.marioto@gmail.com>
2022-03-22 10:50:53 -0700

    Inicial Commit

---
+++ Domain/Produto.cs
@@ -0,0 +1,9 @@
+﻿public class Produto{
+
+    public string Id {get; set;}
+    public string Nome {get; set;}
+    public string Descricao {get; set;}
+    public string Preco {get; set;}
+    public string Categoria {get; set;}
+
+}
